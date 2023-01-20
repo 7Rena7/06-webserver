0acf2c5349be6651a3370acf966d5cfed5a7d5c3
Renato Corbellini <renacorbe@gmail.com>
2023-01-20 13:04:36 -0300

    Add Handlebars functionality

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 const express = require("express");
-const app = express();
-const port = 8080;
+const hbs = require("hbs");
+require("dotenv").config();
 
+const app = express();
+const port = process.env.PORT;
+
+// Handlebars
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+
+// Serve Static Content
 app.use(express.static("public"));
 
-app.get("/Another-Hello-World", (req, res) => {
-  res.send("This is another Hello World!");
+app.get("/", (req, res) => {
+  res.render("home", {
+    name: "Renato Corbellini",
+    title: "This is my first Node App",
+  });
+});
+
+app.get("/generic", (req, res) => {
+  res.render("generic", {
+    name: "Renato Corbellini",
+    title: "This is my first Node App",
+  });
+});
+
+app.get("/elements", (req, res) => {
+  res.render("elements", {
+    name: "Renato Corbellini",
+    title: "This is my first Node App",
+  });
 });
 
 app.get("*", (req, res) => {
